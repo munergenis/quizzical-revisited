bef00cfecbfa192ba3ecdaedde9f32f27cd6308c
Genis Muner <munergenis@gmail.com>
2024-08-13 13:14:30 +0200

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    'src/index.html',
+    'src/**/*.{js,jsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
